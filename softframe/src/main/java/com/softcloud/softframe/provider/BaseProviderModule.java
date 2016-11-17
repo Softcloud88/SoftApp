d6888230b11eb68d50fc09b99aabe4c4298eda9d
renzhexin <renzhexin@nicaifu.com>
2016-11-17 10:59:47 +0800

    MOD: finish OkHttpClientConfig.

--- softframe/src/main/java/com/softcloud/softframe/provider/BaseProviderModule.java
+++ softframe/src/main/java/com/softcloud/softframe/provider/BaseProviderModule.java
@@ -3,10 +3,13 @@ package com.softcloud.softframe.provider;
 import com.facebook.stetho.okhttp3.StethoInterceptor;
 import com.moczul.ok2curl.CurlInterceptor;
 
+import java.util.Collection;
+
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import timber.log.Timber;
@@ -22,11 +25,15 @@ public class BaseProviderModule {
     @Provides
     OkHttpClient provideOkHttpClient(OkHttpClientConfig config) {
         final OkHttpClient.Builder builder = new OkHttpClient.Builder();
-        if (config.logEnable()) {
-            builder.addNetworkInterceptor(new StethoInterceptor())
-                    .addInterceptor(new HttpLoggingInterceptor(message -> Timber.tag("OkHttp").d(message))
-                            .setLevel(HttpLoggingInterceptor.Level.BODY))
-                    .addInterceptor(new CurlInterceptor(message -> Timber.tag("Ok2Curl").d(message)));
+        if (config.netInterceptors() != null) {
+            for (Interceptor netInterceptor : config.netInterceptors()) {
+                builder.addNetworkInterceptor(netInterceptor);
+            }
+        }
+        if (config.appInterceptors() != null) {
+            for (Interceptor appInterceptor : config.appInterceptors()) {
+                builder.addInterceptor(appInterceptor);
+            }
         }
         return builder.build();
     }
--- softframe/src/main/java/com/softcloud/softframe/provider/OkHttpClientConfig.java
+++ softframe/src/main/java/com/softcloud/softframe/provider/OkHttpClientConfig.java
@@ -2,6 +2,10 @@ package com.softcloud.softframe.provider;
 
 import com.google.auto.value.AutoValue;
 
+import java.util.List;
+
+import okhttp3.Interceptor;
+
 /**
  * Created by Softcloud on 16/11/16.
  */
@@ -9,14 +13,27 @@ import com.google.auto.value.AutoValue;
 @AutoValue
 public abstract class OkHttpClientConfig {
     public static Builder builder() {
-        return new AutoValue_OkHttpClientConfig.Builder();
+        return new AutoValue_OkHttpClientConfig.Builder()
+                .setConnectionTimeOutSeconds(10)
+                .setReadTimeOutSeconds(10)
+                .setWriteTimeOutSeconds(10);
     }
 
-    public abstract boolean logEnable();
+    public abstract List<Interceptor> netInterceptors();
+    public abstract List<Interceptor> appInterceptors();
+
+    public abstract int connectionTimeOutSeconds();
+    public abstract int readTimeOutSeconds();
+    public abstract int writeTimeOutSeconds();
 
     @AutoValue.Builder
     public abstract static class Builder {
-        public abstract Builder setLogEnable(final boolean isLogEnableLog);
+        public abstract Builder setNetInterceptors(final List<Interceptor> netInterceptors);
+        public abstract Builder setAppInterceptors(final List<Interceptor> appInterceptors);
+
+        public abstract Builder setConnectionTimeOutSeconds(final int connectionTimeOutSeconds);
+        public abstract Builder setReadTimeOutSeconds(final int readTimeOutSeconds);
+        public abstract Builder setWriteTimeOutSeconds(final int writeTimeOutSeconds);
 
         public abstract OkHttpClientConfig build();
     }
