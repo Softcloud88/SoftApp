20163e483bcbd7801f742d5ce11ef6415b439123
renzhexin <renzhexin@nicaifu.com>
2016-11-29 10:16:22 +0800

    ADD: static type adapter for Mechanic.

--- model/src/main/java/com/softcloud/model/Mechanic.java
+++ model/src/main/java/com/softcloud/model/Mechanic.java
@@ -1,6 +1,8 @@
 package com.softcloud.model;
 
 import com.google.auto.value.AutoValue;
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
 import com.softcloud.model.base.BaseData;
 
 /**
@@ -11,4 +13,8 @@ import com.softcloud.model.base.BaseData;
 public abstract class Mechanic extends BaseData {
 
     public abstract String name();
+
+    public static TypeAdapter<Mechanic> typeAdapter(final Gson gson) {
+        return new AutoValue_Mechanic.GsonTypeAdapter(gson);
+    }
 }
