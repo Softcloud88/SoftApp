18d7538fd03e59db2150970e7bd6dc89937e3947
softcloud <softcloud1988@gmail.com>
2016-11-29 22:12:36 +0800

    ADD: QueryInfoReferanceStore.

---
+++ model/src/main/java/com/softcloud/model/QueryInfoReferanceStore.java
@@ -0,0 +1,19 @@
+package com.softcloud.model;
+
+import java.util.List;
+
+/**
+ * Created by Softcloud on 16/11/29.
+ */
+
+public class QueryInfoReferanceStore {
+    public static final String[] classes = {"德鲁伊", "猎人", "法师", "圣骑士", "牧师", "潜行者", "萨满祭司", "术士", "战士", "Dream"};
+    public static final String[] sets = {"基本", "经典", "纪念", "奖励", "纳克萨玛斯", "地精大战侏儒", "黑石山的火焰", "冠军的试炼", "探险者协会", "上古之神的低语", "卡拉赞", "乱斗模式", "Hero Skins", "Missions", "Credits", "System", "Debug"};
+    public static final String[] standard = {"基本", "经典", "黑石山的火焰", "冠军的试炼", "探险者协会", "上古之神的低语", "卡拉赞"};
+    public static final String[] wild = {"基本", "经典", "纪念", "奖励", "纳克萨玛斯", "地精大战侏儒", "黑石山的火焰", "冠军的试炼", "探险者协会", "上古之神的低语", "卡拉赞"};
+    public static final String[] types = {"英雄", "随从", "法术", "强化", "武器", "英雄技能"};
+    public static final String[] factions = {"Horde", "Alliance", "Neutral"};
+    public static final String[] qualities = {"免费", "普通", "稀有", "史诗", "传说"};
+    public static final String[] races = {"恶魔", "龙", "机械", "鱼人", "野兽", "海盗", "图腾"};
+    public static final String[] locales = {"deDE", "enGB", "enUS", "esES", "esMX", "frFR", "itIT", "koKR", "plPL", "ptBR", "ruRU", "zhCN", "zhTW", "jaJP", "thTH"};
+}
